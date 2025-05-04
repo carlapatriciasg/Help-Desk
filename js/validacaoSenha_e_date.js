0bdf83b3bca43bf2e8b4a179c615e2f382d90573
kaiquetest <kaiquesilvaaquino2@gmail.com>
2025-05-04 14:56:26 -0300

    validacaoSenha_e_date

--- js/validacaoSenha_e_date.js
+++ js/validacaoSenha_e_date.js
@@ -49,3 +49,25 @@ function validarConfirmacaoSenha() {
   }
 }
 
+// validacao data 
+document.querySelector('input[type="date"]').addEventListener('input', function () {
+  const data = this.value;
+
+  // Verifica se o formato da data está correto (YYYY-MM-DD)
+  const regex = /^\d{4}-\d{2}-\d{2}$/;
+
+  if (!regex.test(data)) {
+    this.value = ''; // Limpa o campo se o formato estiver incorreto
+    alert('Data inválida! Certifique-se de usar o formato correto: DD/MM/AAAA.');
+    return;
+  }
+
+  // Extrai o ano da data
+  const ano = data.split('-')[0];
+
+  // Verifica se o ano tem exatamente 4 dígitos
+  if (ano.length !== 4) {
+    this.value = ''; // Limpa o campo se o ano não tiver 4 dígitos
+    alert('O ano deve conter exatamente 4 dígitos.');
+  }
+});
