e97aa28745d37e5621d968de2c3089f192d9f08c
vinicastro15 <vc9410617@gmail.com>
2025-05-08 22:50:04 -0300

    implementação api-Vlibras

---
+++ js/sidebar.js
@@ -0,0 +1,59 @@
+//<!-- fechar a sidebar -->
+document.addEventListener('DOMContentLoaded', function () {
+    const sidebarToggle = document.querySelector('.sidebar-toggle');
+    const sidebar = document.querySelector('.sidebar');
+    const sidebarBackdrop = document.querySelector('#sidebarBackdrop');
+
+    if (sidebarToggle && sidebar && sidebarBackdrop) {
+        sidebarToggle.addEventListener('click', function () {
+            sidebar.classList.toggle('show');
+            sidebarBackdrop.classList.toggle('show');
+        });
+
+        sidebarBackdrop.addEventListener('click', function () {
+            sidebar.classList.remove('show');
+            sidebarBackdrop.classList.remove('show');
+        });
+    } else {
+        console.error('Elementos necessários para a funcionalidade da sidebar não foram encontrados.');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const sidebarCollapseButton = document.querySelector('.sidebar-collapse');
+    const sidebar = document.querySelector('.sidebar');
+    const mainContentWrapper = document.querySelector('.main-content-wrapper');
+
+    if (sidebarCollapseButton && sidebar && mainContentWrapper) {
+        sidebarCollapseButton.addEventListener('click', function () {
+            sidebar.classList.toggle('collapsed');
+            mainContentWrapper.classList.toggle('collapsed');
+        });
+    } else {
+        console.error('Elementos necessários para a funcionalidade de recolher a sidebar não foram encontrados.');
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Logout button na sidebar
+    const logoutSidebarButton = document.getElementById('logout-button-sidebar');
+    if (logoutSidebarButton) {
+        logoutSidebarButton.addEventListener('click', function (e) {
+            e.preventDefault();
+            if (confirm('Tem certeza de que deseja sair?')) {
+                window.location.href = 'index.html';
+            }
+        });
+    }
+
+    // Logout button no dropdown do cabeçalho
+    const logoutHeaderButton = document.getElementById('logout-button');
+    if (logoutHeaderButton) {
+        logoutHeaderButton.addEventListener('click', function (e) {
+            e.preventDefault();
+            if (confirm('Tem certeza de que deseja sair?')) {
+                window.location.href = 'index.html';
+            }
+        });
+    }
+});
