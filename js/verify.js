00083414a643657026c823b36ab988ce2c8fb6df
Carla Patrícia <carlapatriciasg@gmail.com>
2025-05-12 21:08:59 -0300

    input visualizar senha + script

--- js/verify.js
+++ js/verify.js
@@ -162,3 +162,18 @@ class CepConsulta {
 function pesquisacep(valor) {
     CepConsulta.pesquisar(valor);
 }
+
+//Visualização de senha
+  function togglePassword(fieldId, button) {
+    const input = document.getElementById(fieldId);
+    const icon = button.querySelector('i');
+    if (input.type === 'password') {
+      input.type = 'text';
+      icon.classList.remove('bi-eye');
+      icon.classList.add('bi-eye-slash');
+    } else {
+      input.type = 'password';
+      icon.classList.remove('bi-eye-slash');
+      icon.classList.add('bi-eye');
+    }
+  }
