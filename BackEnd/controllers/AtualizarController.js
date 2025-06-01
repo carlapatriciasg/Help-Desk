94281bd16d4f3f71d0704524f235065bf74562d5
caique mendes <caiquemendes013@gmail.com>
2025-06-01 12:55:23 -0300

    Implementação do esqueci senha

--- BackEnd/controllers/AtualizarController.js
+++ BackEnd/controllers/AtualizarController.js
@@ -1,6 +1,7 @@
 const Usuario = require('../models/usuarioModelo')
 const Analista = require('../models/AnalistaModelo')
 const bcrypt = require('bcryptjs')
+const validator = require('validator')
 const { where } = require('sequelize')
 
 exports.atualizarDados = async (req, res) =>{
@@ -34,6 +35,40 @@ exports.atualizarDados = async (req, res) =>{
     res.json({ msg: 'Dados atualizados' })
 }
 
+exports.esqueciSenha = async (req, res) =>{
+    const { email, confirmSenha, senhaNova } = req.body
+
+    let usuario = await Usuario.findOne({where: {email}})
+    if (!usuario) {
+        return res.status(404).json({ msg: 'Usuário não encontrado' });
+    }
+    
+    try {
+    if(!senhaNova){
+        return res.status(422).json({msg: 'A senha é obrigatório'})
+    }
+    if(!confirmSenha){
+        return res.status(422).json({msg: 'A senha de confirmação é obrigatório'})
+    }
+    if(!validator.isLength(senhaNova, {min: 8, max: 20})){
+            return res.status(422).json({msg: 'A senha deve ter de 8 a 20 caracteres'})
+    }
+    if(!validator.isAlphanumeric(senhaNova)){
+        return res.status(422).json({msg: 'A senha deve conter letras e numeros'})
+    }
+    if(senhaNova !== confirmSenha){
+       return res.status(422).json({msg: 'As senhas não conferem '})
+    }
+
+    usuario.senha = await bcrypt.hash(senhaNova, 12)
+    await usuario.save()
+
+    res.json({ msg: 'Dados atualizados' })
+    }catch(erro){
+        console.error('Erro ao redefinir senha: ', erro)
+        return res.status(500).json({ msg: 'Erro interno do servidor' })
+    }
+}
 exports.perfilDados = async (req, res) =>{
     const { email } = req.query
 
